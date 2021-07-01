490a13e88c58260d27d9f21bdd30416b28ae783e
Felipe-M-dev <39914824+Felipe-M-dev@users.noreply.github.com>
2021-06-30 21:24:59 -0400

    Update numeros.rb

--- numeros.rb
+++ numeros.rb
@@ -1,9 +1,13 @@
 num = ARGV[0].to_i
-collector = ''
-n = 1
 
-while n <= num
-    collector += "#{n}"
-    puts "#{collector}"
-    n += 1
+if ARGV.empty?
+    puts "Ingrese un argumento numerico (Int)."
+else
+    collector = ''
+    n = 1
+    while n <= num
+        collector += "#{n}"
+        puts "#{collector}"
+        n += 1
+    end
 end
